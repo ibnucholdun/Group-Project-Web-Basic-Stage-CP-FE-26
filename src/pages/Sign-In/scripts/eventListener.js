5b876be07eacc99c08adee2d1cf3fc8a8e5e894a
Ibnu Choldun <96803344+ibnucholdun@users.noreply.github.com>
2022-10-22 10:23:16 +0700

    Revert "update logic sign in"

--- src/pages/Sign-In/scripts/eventListener.js
+++ src/pages/Sign-In/scripts/eventListener.js
@@ -1,21 +1,9 @@
-import postSignIn from '../scripts/postSignIn.js';
-
 const eventListener = () => {
-    const form = document.querySelector('form');
-
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        event.stopPropagation();
-
-        if (form.checkValidity() === true) {
-            const user = await postSignIn();
-            if (user) {
-                window.location.href = '../Home-Page/home.html';
-            } else {
-                alert('Email atau password salah');
-            }
-        }
-    });
+    const buttonSignIn = document.querySelector(".btn-sign-in");
+    buttonSignIn.addEventListener("click", (e) => {
+        e.preventDefault();
+        window.location.href = "../Home-Page/home.html";
+    })
 };
 
 export default eventListener;
